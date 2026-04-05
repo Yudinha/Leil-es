058ad6f83a86a3aae4f5db0758964bacedd08c02
Yuda <dlvdavidlimavieira@gmail.com>
2026-04-05 03:03:50 -0300

    Mensagem de sucesso ao cadastrar produto

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -151,6 +151,7 @@ public class cadastroVIEW extends javax.swing.JFrame {
         ProdutosDAO produtodao = new ProdutosDAO();
         produtodao.cadastrarProduto(produto);
         System.out.println("Botão cadastrar clicado!");
+        javax.swing.JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
     private void btnProdutosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProdutosActionPerformed
